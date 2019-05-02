e1a1709bfb7bbd7364b43439b4b4c573b851fd83
Brian Wong <brianwong@zipcoders-MBP.fios-router.home>
2019-05-02 00:05:46 -0400

    Created Spring Boot Initializer

---
+++ src/main/java/com/bwong/projectjoker/ProjectJokerApplication.java
@@ -0,0 +1,13 @@
+package com.bwong.projectjoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJokerApplication.class, args);
+	}
+
+}
